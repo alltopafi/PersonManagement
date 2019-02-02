e1faf950309d50e0ff9bc2c68d66fc16127b3bd1
Jesse Alltop <alltopafi@gmail.com>
2019-02-01 19:23:48 -0600

    update comment

--- src/main/java/alltop/personmanagement/personManagement/App.java
+++ src/main/java/alltop/personmanagement/personManagement/App.java
@@ -8,11 +8,12 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-       //Let's assume that the first arg is the file name and 
+       //Let's assume that the only arg is the file name and 
     	if(args.length != 1) {
     		throw new Exception("Error: expected file to be passed as argument to read data from.");
     	}
-    
-    
+        
     }
+    
+   
 }
