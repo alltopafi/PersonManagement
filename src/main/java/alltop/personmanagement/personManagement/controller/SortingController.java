168f27a60b5e4b8ee7f9f1ab60f8c235420bc863
Jesse Alltop <alltopafi@gmail.com>
2019-02-03 13:52:47 -0600

    update sorting controller to only have one public method that will decide which sorting method to call

--- src/main/java/alltop/personmanagement/personManagement/controller/SortingController.java
+++ src/main/java/alltop/personmanagement/personManagement/controller/SortingController.java
@@ -12,8 +12,21 @@ public class SortingController {
 //	 Output 2 – sorted by birth date, ascending.
 //	 Output 3 – sorted by last name, descending.
 	
+	public void handleSortOption(Person[] people, int choice) throws Exception {
+		
+		switch(choice) {
+		case 1: sortByGender(people);
+				break;
+		case 2: sortByBirthDate(people);
+				break;
+		case 3: sortByLastNameDescending(people);
+				break;
+		default: //this should not be possible by included for test coverage
+			throw new Exception("Invalid input - needed to be 1, 2, or 3");
+		}
+	}
 	
-	public void sortByGender(Person[] people) {
+	private void sortByGender(Person[] people) {
 		Arrays.sort(people, new SortByGender());
 		
 		for(Object object : people) {
@@ -21,7 +34,7 @@ public class SortingController {
 		}
 	}
 	
-	public void sortByBirthDate(Person[] people) {
+	private void sortByBirthDate(Person[] people) {
 		Arrays.sort(people, new SortByBirthDate());
 		
 		for(Object object : people) {
@@ -30,7 +43,7 @@ public class SortingController {
 	}
 	
 
-	public void sortByLastNameDescending(Person[] people) {
+	private void sortByLastNameDescending(Person[] people) {
 		Arrays.sort(people, new SortByLastName());
 		
 		for(Object object : people) {
