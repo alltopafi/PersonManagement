7fd33c965de9c13f5a71d2f733a2deb25cdeed2b
Jesse Alltop <alltopafi@gmail.com>
2019-02-03 21:20:52 -0600

    move data file to bean so that we can inject test data

--- sprintboot/personmanagement/src/main/java/alltop/personmanagement/controller/PersonManagementController.java
+++ sprintboot/personmanagement/src/main/java/alltop/personmanagement/controller/PersonManagementController.java
@@ -1,7 +1,10 @@
 package alltop.personmanagement.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Set;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +25,15 @@ public class PersonManagementController {
 	private final SortingController sortingController = new SortingController();
 	private final ObjectMapper mapper = new ObjectMapper();
 
+
+    @Bean(name="personData")
+    public File namedFile() throws FileNotFoundException {
+        File namedFile = ResourceUtils.getFile("classpath:personData.txt");
+        return namedFile;
+    }
+	
+	
+	
 //	POST /records - Post a single data line in any of the 3 formats supported by your existing code
 //	GET /records/gender - returns records sorted by gender
 //	GET /records/birthdate - returns records sorted by birthdate
@@ -29,7 +41,7 @@ public class PersonManagementController {
 
 	@RequestMapping(value="/records/gender", method = RequestMethod.GET)
 	public String returnSortedByGender() throws Exception {
-		String path = ResourceUtils.getFile("classpath:personData.txt").getPath();
+		String path = namedFile().getPath();
 		Set<String> rawDataFromFile = fileReaderController.readFile(path);
 		Set<Person> people = personTransformer.transformPersonsFromRawData(rawDataFromFile);
 		Person[] temp = people.toArray(new Person[people.size()]);
---
+++ sprintboot/personmanagement/src/test/java/alltop/personmanagement/controller/PersonManagementController.java
@@ -0,0 +1,11 @@
+package alltop.personmanagement.controller;
+
+import org.junit.Test;
+
+public class PersonManagementController {
+
+	@Test
+	public void testReturnSortByGender() {
+		
+	}
+}
